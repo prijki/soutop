c8a3effce28846572c1f19b6b2913d938fd0efee
soutop <priscila.czki@gmail.com>
2022-06-29 11:46:17 -0300

    Commit inicial

---
+++ core/admin.py
@@ -0,0 +1,7 @@
+from re import M
+from django.contrib import admin
+from core.models import Categoria, Editora, Marca
+
+admin.site.register(Categoria)
+admin.site.register(Editora)
+admin.site.register(Marca)
