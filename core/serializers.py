53ddc238275fb13f5bea21e8680046ac35296612
soutop <priscila.czki@gmail.com>
2022-07-13 11:57:17 -0300

    13 07

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework.serializers import ModelSerializer
-
-from core.models import Categoria
+from core.models import Categoria, Editora
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
         fields = "__all__"
+
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
+        field = "__all__"
