0f401f175c384873b185fa6e20f5a2e482caff0a
soutop <priscila.czki@gmail.com>
2022-07-13 11:59:31 -0300

    13 07

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 
-from core.models import Categoria
+from core.models import Categoria, Editora
 from core.serializers import CategoriaSerializer, EditoraSerializer
 
 class CategoriaViewSet(ModelViewSet):
